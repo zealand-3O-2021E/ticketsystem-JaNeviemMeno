11ae8fee0e2eeb88c7672e2cd0597d301dbe806e
JaNeviemMeno <75016040+JaNeviemMeno@users.noreply.github.com>
2021-09-22 19:24:29 +0200

    task 3

---
+++ ClassLibraryTicketSystem/MC.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem
+{
+    /// <summary>
+    /// A class MC which i suppose is for a motorcycle :D 
+    /// </summary>
+    public class MC
+    {
+        public string Licenseplate { get; set; }
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Method which returns price which is 125
+        /// </summary>
+        /// <returns>125</returns>
+        public double Price()
+        {
+            return 125;
+        }
+
+        /// <summary>
+        /// Method which returns the type of Vehicle
+        /// </summary>
+        /// <returns>MC as string</returns>
+        public string Vehicle()
+        {
+            return "MC";
+        }
+
+
+
+
+    }
+}
---
+++ ClassLibraryTicketSystemTests/MCTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryTicketSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void PriceTest()
+        {
+            MC motor = new();
+            var value = motor.Price();
+            Assert.AreEqual(125,value);
+        }
+
+        [TestMethod()]
+        public void VehicleTest()
+        {
+            MC motor = new();
+            var value = motor.Vehicle();
+            Assert.AreEqual("MC", value);
+        }
+    }
+}
