ae000733c8b312ec77a56a7d665f49f4c512f8bc
JaNeviemMeno <75016040+JaNeviemMeno@users.noreply.github.com>
2021-09-22 19:17:25 +0200

    Task 2

---
+++ ClassLibraryTicketSystem/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassLibraryTicketSystem
+{
+    /// <summary>
+    /// This is our Car class
+    /// </summary>
+    public class Car
+    {
+        public string Licenseplate { get; set; }
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Method which returns price which is 240
+        /// </summary>
+        /// <returns>240</returns>
+        public double Price()
+        {
+            return 240;
+        }
+
+        /// <summary>
+        /// Method which returns the type of Vehicle
+        /// </summary>
+        /// <returns>Car as string</returns>
+        public string VehicleType()
+        {
+            return "car";
+        }
+
+
+
+    }
+}
---
+++ ClassLibraryTicketSystemTests/CarTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryTicketSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Car car = new();
+            var value = car.Price();
+            Assert.AreEqual(240,value);
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            Car car = new();
+            var value = car.VehicleType();
+            Assert.AreEqual("car",value);
+        }
+    }
+}
