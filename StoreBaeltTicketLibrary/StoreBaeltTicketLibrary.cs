2b666aa8f3b1967b137e77f86beac4cbdada69bd
JaNeviemMeno <75016040+JaNeviemMeno@users.noreply.github.com>
2021-09-23 12:44:44 +0200

    task 9

--- StoreBaeltTicketLibrary/StoreBaeltTicketLibrary.cs
+++ StoreBaeltTicketLibrary/StoreBaeltTicketLibrary.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using ClassLibraryTicketSystem;
+using System;
+using System.Collections.Generic;
 
 namespace StoreBaeltTicketLibrary
 {
     public class StoreBaeltTicketLibrary
     {
+        public List<Vehicle> ListOfVehicles;
+        public enum DayOfWeek 
+        { 
+         Monday,
+         Tuesday,
+        Wednesday
+        
+        
+        
+        
+        };
 
+        public StoreBaeltTicketLibrary()
+        {
+            ListOfVehicles = new();
+        }
+        public void WeekendDiscount()
+        {
+            foreach (var item in ListOfVehicles)
+            {
+                if (item.VehicleType()=="car")  
+                {
+                    if (item.Date.Day.ToString()=="Sunday" || item.Date.Day.ToString() == "Saturday")
+                    {
+                        var newPrice = item.Price() - item.Price() * 0.2;
+                        item.Discount(newPrice);
+                       
+                    }
+                }
+            }
+        }
 
     }
 }
